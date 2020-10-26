5538a5a911834c44cdc68ed41166a3d59e631f50
carol <abc61958761@gmail.com>
2020-10-26 17:57:34 +0800

    Update Task.vue

--- web/src/components/Task.vue
+++ web/src/components/Task.vue
@@ -1,173 +1,51 @@
 <template>
-    <div class="task">
-        <TodoTask :todoTasks=todoTasks :todoTaskArgs=todoTaskArgs 
-            @loadDataTodoTask="loadDataTodoTask" 
-            @updateTask="updateTask"
-        />
-        <DoneTask :doneTasks=doneTasks :doneTaskArgs=doneTaskArgs @loadDataDoneTask="loadDataDoneTask" />
-            
-        <v-fab-transition>
-            <v-btn
-                fab
-                large
-                bottom
-                right
-                fixed
-                dark
-                color="orange darken-2" @click="addTaskDialog = true"
-            >
-                <v-icon>mdi-plus</v-icon>
-            </v-btn>
-        </v-fab-transition>
-        <v-dialog persistent v-model="addTaskDialog" max-width="400">
-            <v-card>
-                <v-card-title>
-                    <span>New Task</span>
-                    <v-spacer></v-spacer>
-                    <v-btn icon @click="addTaskDialog = false">
-                        <v-icon>mdi-close</v-icon>
-                    </v-btn>
-                </v-card-title>
-                <v-divider></v-divider>
-                <v-card-text>
-                    <v-text-field label="Name" v-model="newTask.title"></v-text-field>
-                    <v-textarea hide-details outlined v-model="newTask.content"></v-textarea>
-                </v-card-text>
-                <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn color="orange" text @click="createTask">
-                        CREATE
-                    </v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-dialog>
-    </div>
+    <v-list two-line>
+        <v-subheader><slot name="title"></slot></v-subheader>
+        <v-divider></v-divider>
+        <v-list-item-group v-if="taskList">
+            <template v-for="(task, index) in taskList.tasks">
+                <v-list-item v-if="taskList.tasks.length - 1 > index" :key="task._id">
+                    <v-list-item-content>
+                        <v-list-item-title>{{task.title}}</v-list-item-title>
+                        <v-list-item-subtitle>{{task.content}}</v-list-item-subtitle>
+                    </v-list-item-content>
+                    <v-list-item-action>
+                        <slot name="action" :task="task"></slot>
+                    </v-list-item-action>
+                </v-list-item>
+
+                <v-list-item v-else :key="task._id" v-intersect="loadDataTask" >
+                    <v-list-item-content>
+                        <v-list-item-title>{{task.title}}</v-list-item-title>
+                        <v-list-item-subtitle>{{task.content}}</v-list-item-subtitle>
+                    </v-list-item-content>
+                    <v-list-item-action>
+                        <slot name="action" :task="task"></slot>
+                    </v-list-item-action>
+                </v-list-item>
+            </template>
+        </v-list-item-group>
+    </v-list>
 </template>
 
 <script>
-import { TODO_TASKS, DONE_TASKS, UPDATE_TASK, CREATE_TASK } from '../schemas/Task';
-import TodoTask from './TodoTask';
-import DoneTask from './DoneTask';
-
 export default {
-    data: () => {
-        return {
-            addTaskDialog: false,
-            newTask: {
-                title: null,
-                content: null
-            },
-            initPageArgs: {
-                start: 0,
-                pageSize: 10,
-                page: 0
-            },
-            doneTaskArgs: {},
-            todoTaskArgs: {}
-        }
-    },
-    components: { TodoTask, DoneTask },
-    created() {
-        Object.assign(this.doneTaskArgs, this.initPageArgs);
-        Object.assign(this.todoTaskArgs, this.initPageArgs);
-    },
+    props: ['taskList', 'name'],
     methods: {
-        createTask() {
-            this.$apollo.mutate({
-                mutation: CREATE_TASK,
-                variables: {
-                    taskData: this.newTask
-                },
-                update: () => {
-                    this.addTaskDialog = false;
-                    this.$apollo.queries.todoTasks.refetch()
-                    Object.assign(this.todoTaskArgs, this.initPageArgs);
-
-                    this.newTask = {
-                        title: null,
-                        content: null
-                    }
-                }
-            })
-        },
-        async updateTask(data) {
-            await this.$apollo.mutate({
-                mutation: UPDATE_TASK,
-                variables: data
-            })
-            this.$apollo.queries.todoTasks.refetch()
-            this.$apollo.queries.doneTasks.refetch()
-
-            Object.assign(this.doneTaskArgs, this.initPageArgs);
-            Object.assign(this.todoTaskArgs, this.initPageArgs);
-        },
-        loadDataDoneTask() {
-            const currentPageCount = this.doneTaskArgs.pageSize * this.doneTaskArgs.page;
-
-            this.doneTaskArgs.start = currentPageCount + this.doneTaskArgs.pageSize;
-            this.$apollo.queries.doneTasks.fetchMore({
-                // New variables
-                variables: this.doneTaskArgs,
-                // Transform the previous result with new data
-                updateQuery: (previousResult, { fetchMoreResult, variables }) => {
-                    previousResult.doneTasks.tasks.push(...fetchMoreResult.doneTasks.tasks);
-                    previousResult.doneTasks.taskCount = fetchMoreResult.doneTasks.taskCount;
-
-                    return {
-                        doneTasks: previousResult.doneTasks
-                    }
-                },
-            })
-
-            this.doneTaskArgs.page ++;
-        },
-        loadDataTodoTask(entries) {
-            const currentPageCount = this.todoTaskArgs.pageSize * this.todoTaskArgs.page;
-
-            this.todoTaskArgs.start = currentPageCount + this.todoTaskArgs.pageSize;
-            this.$apollo.queries.todoTasks.fetchMore({
-                // New variables
-                variables: this.todoTaskArgs,
-                // Transform the previous result with new data
-                updateQuery: (previousResult, { fetchMoreResult, variables }) => {
-                    previousResult.todoTasks.tasks.push(...fetchMoreResult.todoTasks.tasks);
-                    previousResult.todoTasks.taskCount = fetchMoreResult.todoTasks.taskCount;
-
-                    return {
-                        todoTasks: previousResult.todoTasks
-                    }
-                },
-            })
-
-            this.todoTaskArgs.page ++;
-        }
-     },
-     apollo: {
-         todoTasks: {
-            query: TODO_TASKS
-        },
-        doneTasks: {
-            query: DONE_TASKS
+        loadDataTask(entries, observer, isIntersecting) {
+            if (!isIntersecting) return;
+            if (this.taskList.taskCount == 0 || this.taskList.tasks.length === this.taskList.taskCount) return;
+            switch (this.name) {
+                case 'todo':
+                    this.$emit('loadDataTodoTask');
+                    break;
+                case 'done':
+                    this.$emit('loadDataDoneTask');
+                    break;
+                default:
+                    break;
+            }
         }
      }
 }
 </script>
-
-<style lang="scss">
-.task {
-    display: flex;
-    justify-content: space-around;
-    flex: 1;
-    height: 100vh;
-    > .v-list {
-        flex: 1;
-        margin: 20px;
-
-        overflow: scroll;
-        .v-list-item-group {
-            overflow: scroll;
-        }
-    }
-}
-
-</style>
